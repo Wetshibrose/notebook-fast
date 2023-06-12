36cb664ad98e0fbf8612697e0e45ce0019d6004e
Jaydrey <jaribwetshi64@gmail.com>
2023-06-12 17:26:28 +0300

    feat(project main): query using the raw sql db connections

--- app/main.py
+++ app/main.py
@@ -1,22 +1,35 @@
 from fastapi import FastAPI, status, HTTPException
 
+
+
 # data
 from .data import notes
 from .schema import Note
+from .db_raw import (
+    instantiate_db,
+    get_notes_db,
+    add_note_db,
+    get_note_db,
+    delete_note_db,
+    change_note_db
+)
 
 app = FastAPI()
+
+# instantiate db and tables
+instantiate_db()
 
 
 @app.get("/", status_code=status.HTTP_200_OK)
 def get_notes():
-    all_notes: list[Note] = notes
-    return {"notes": all_notes}
+    all_notes = get_notes_db()
+    return {"data": all_notes}
 
 
 @app.get("/notes", status_code=status.HTTP_200_OK)
 def get_notes():
-    all_notes: list[Note] = notes
-    return {"notes": all_notes}
+    all_notes = get_notes_db()
+    return {"data": all_notes}
 
 
 @app.post("/notes", status_code=status.HTTP_200_OK)
@@ -24,8 +37,12 @@ def add_note(note: Note):
     if not note:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST, detail="note body required")
-    notes.append(note)
-    return {"note": note}
+    new_note = add_note_db(note=note)
+    if isinstance(new_note, str) or new_note is None:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"{new_note}")
+
+    return {"data": new_note}
 
 
 @app.get("/notes/{note_id}", status_code=status.HTTP_200_OK)
@@ -33,20 +50,13 @@ def get_note(note_id: str):
     if not note_id:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST, detail="note id is required",)
-    note_id = note_id.replace("-", "")
-    print(note_id)
-    note: Note = None
-    for note_v in notes:
-        print(note_v.id.hex)
-        if note_v.id.hex == note_id:
-            note = note_v
-            break
 
-    if note is None:
+    note: Note = get_note_db(id=note_id)
+    if isinstance(note, str) or note is None:
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="note not found",)
+            status_code=status.HTTP_404_NOT_FOUND, detail=f"{note}",)
 
-    return {"note": note}
+    return {"data": note}
 
 
 @app.put("/notes/{note_id}", status_code=status.HTTP_200_OK)
@@ -54,7 +64,11 @@ def change_note(note_id: str, note: Note):
     if not note:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST, detail="bad format of the body")
-    notes.append(note)
+    note = change_note_db(note=note)
+    if isinstance(note, str):
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail=f"{note}",)
+
     return {"note": note}
 
 
@@ -64,14 +78,7 @@ def delete_note(note_id: str):
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST, detail="note id is missing")
 
-    note_id = note_id.replace("-", "")
-    note_index: int = None
-    for index, note in enumerate(notes):
-        if note.id.hex == note_id:
-            note_index = index
-            break
-
-    if not note_index:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"note with the given id {note_id} is not found")
-    notes.pop(note_index)
+    note = delete_note_db(id=note_id)
+    if isinstance(note, str):
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail=f"{note}")
